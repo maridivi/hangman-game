d08b55038a5f12e809fd03b47ca03dc5bc4b3a07
Marianna Di Vito <maridivi89@gmail.com>
2024-01-30 15:18:04 +0000

    Increase counter if guess is not correct

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ class Game {
   constructor() {
     this.currentWord = difficultWords[getRandomIndex(difficultWords)];
     this.displayHiddenWord();
+    this.wrongGuessesCounter = 0;
   }
   displayHiddenWord() {
     for (let i = 0; i < this.currentWord.length; i++) {
@@ -13,25 +14,28 @@ class Game {
       //   const char = document.createElement("p");
       const wordContainer = document.getElementById("word");
       hiddenChar.innerText = "_";
-      //   char.innerText = this.currentWord[i];
-      //   char.style.display = "none";
+
       hiddenChar.classList.add("character");
       wordContainer.appendChild(hiddenChar);
-      //   wordContainer.appendChild(char);
     }
   }
 
   displayIncludedCharacter(selectedLetter) {
     const currentWord = game1.currentWord;
+
+    if (!currentWord.toUpperCase().includes(selectedLetter.toUpperCase())) {
+      game1.increaseGuessesCounter();
+    }
     for (let i = 0; i < currentWord.length; i++) {
       if (currentWord[i].toUpperCase() === selectedLetter.toUpperCase()) {
-        console.log(i);
         const charElements = document.getElementsByClassName("character");
         charElements[i].innerHTML = currentWord[i];
-      } else {
-        console.log("Wrong");
       }
     }
+  }
+  increaseGuessesCounter() {
+    this.wrongGuessesCounter++;
+    console.log(this.wrongGuessesCounter);
   }
 }
 
@@ -42,10 +46,7 @@ const letterButtons = document.querySelectorAll("#letter-button");
 letterButtons.forEach((button) =>
   button.addEventListener("click", (e) => {
     const selectedLetter = e.target.textContent;
-    // if (game1.currentWord.includes(selectedLetter)) {
-    //   console.log(selectedLetter);
-    //   console.log(game1.currentWord);
-    // }
+
     game1.displayIncludedCharacter(selectedLetter);
   })
 );
