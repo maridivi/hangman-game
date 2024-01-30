20e134a960859601d36574300a9814cdd0c52f77
Marianna Di Vito <maridivi89@gmail.com>
2024-01-30 16:47:50 +0000

    Start drawing hangman for incorrect guesses

--- js/index.js
+++ js/index.js
@@ -25,6 +25,7 @@ class Game {
 
     if (!currentWord.toUpperCase().includes(selectedLetter.toUpperCase())) {
       game1.increaseGuessesCounter();
+      game1.displayHangmanParts();
     }
     for (let i = 0; i < currentWord.length; i++) {
       if (currentWord[i].toUpperCase() === selectedLetter.toUpperCase()) {
@@ -36,6 +37,58 @@ class Game {
   increaseGuessesCounter() {
     this.wrongGuessesCounter++;
     console.log(this.wrongGuessesCounter);
+  }
+  displayHangmanParts() {
+    const hangmanContainer = document.getElementById("hangman");
+    const counter = game1.wrongGuessesCounter;
+
+    switch (counter) {
+      case 1:
+        const verticalLine = document.createElement("div");
+        verticalLine.style.height = "100%";
+        verticalLine.style.width = "3px";
+        verticalLine.style.backgroundColor = "black";
+
+        hangmanContainer.appendChild(verticalLine);
+        console.log(verticalLine);
+
+        break;
+      case 2:
+        const horizontalLine = document.createElement("div");
+        horizontalLine.style.height = "3px";
+        horizontalLine.style.width = "80%";
+        horizontalLine.style.backgroundColor = "black";
+        horizontalLine.style.position = "absolute";
+        horizontalLine.style.top = "0";
+
+        hangmanContainer.appendChild(horizontalLine);
+        break;
+      case 3:
+        const shortVerticalLine = document.createElement("div");
+        shortVerticalLine.style.height = "25%";
+        shortVerticalLine.style.width = "3px";
+        shortVerticalLine.style.backgroundColor = "black";
+        shortVerticalLine.style.position = "absolute";
+        shortVerticalLine.style.right = "20%";
+        shortVerticalLine.style.top = "0";
+
+        hangmanContainer.appendChild(shortVerticalLine);
+        break;
+      case 4:
+        const head = document.createElement("div");
+        head.style.height = "30px";
+        head.style.width = "30px";
+        head.style.border = "1px solid black";
+        head.style.borderRadius = "100%";
+        head.style.position = "absolute";
+        head.style.right = "13%";
+        head.style.top = "25%";
+
+        hangmanContainer.appendChild(head);
+        break;
+      default:
+      // code block
+    }
   }
 }
 
