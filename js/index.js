77f20aad02f9be7b7779ce5a0b7a008b790dcefe
Marianna Di Vito <maridivi89@gmail.com>
2024-01-30 14:24:45 +0000

    Display hidden word and buttons with characters

--- js/index.js
+++ js/index.js
@@ -10,11 +10,38 @@ class Game {
   displayHiddenWord() {
     for (let i = 0; i < this.currentWord.length; i++) {
       const hiddenChar = document.createElement("p");
+      //   const char = document.createElement("p");
       const wordContainer = document.getElementById("word");
       hiddenChar.innerText = "_";
+      //   char.innerText = this.currentWord[i];
+      //   char.style.display = "none";
+      hiddenChar.classList.add("character");
       wordContainer.appendChild(hiddenChar);
+      //   wordContainer.appendChild(char);
+    }
+  }
+
+  displayIncludedCharacter(selectedLetter) {
+    if (
+      game1.currentWord.toUpperCase().includes(selectedLetter.toUpperCase())
+    ) {
+      const charElements = document.getElementsByClassName("character");
+      console.log(charElements);
     }
   }
 }
 
 const game1 = new Game();
+console.log(game1.currentWord);
+
+const letterButtons = document.querySelectorAll("#letter-button");
+letterButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    const selectedLetter = e.target.textContent;
+    // if (game1.currentWord.includes(selectedLetter)) {
+    //   console.log(selectedLetter);
+    //   console.log(game1.currentWord);
+    // }
+    game1.displayIncludedCharacter(selectedLetter);
+  })
+);
